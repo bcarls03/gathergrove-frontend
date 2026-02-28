61328d5865576a4e10bd08b53ba80d1bac04c036
bcarls03 <brian.carlberg@gmail.com>
2026-02-28 08:16:45 -0500

    ComposePost: swap Text Invite emoji for SVG icon

--- src/pages/ComposePost.tsx
+++ src/pages/ComposePost.tsx
@@ -1212,7 +1212,21 @@ export default function ComposePost() {
                   e.currentTarget.style.backgroundColor = "#3b82f6";
                 }}
               >
-                <span style={{ fontSize: "18px" }}>📱</span>
+                <span style={{ display: "flex", alignItems: "center" }}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.79 19.79 0 0 1 2.09 4.18 2 2 0 0 1 4.06 2h3a2 2 0 0 1 2 1.72c.12.86.37 1.7.73 2.5a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.58-1.25a2 2 0 0 1 2.11-.45c.8.36 1.64.61 2.5.73A2 2 0 0 1 22 16.92z" />
+                  </svg>
+                </span>
                 Text Invite
               </button>
 
